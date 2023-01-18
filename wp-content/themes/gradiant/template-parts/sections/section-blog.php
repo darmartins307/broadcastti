46d5f8939987d6ab41218953e2eb90c449f62779
João Martins <78485143+darmartins307@users.noreply.github.com>
2023-01-18 18:24:30 -0300

    Iniciando as colunas

---
+++ wp-content/themes/gradiant/template-parts/sections/section-blog.php
@@ -0,0 +1,9 @@
+<div class="container">
+  <div class="row">
+    <div class="col" style="background-color: black;">
+      1 de 2
+    </div>
+    <div class="col">
+      2 de 2
+    </div>
+  </div>
